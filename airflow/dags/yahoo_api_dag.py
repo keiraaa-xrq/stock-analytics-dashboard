708e6997bf88cef493b52fec613f764d4e8f4982
Feng Yuan <gdfy102217@gmail.com>
2023-04-03 15:51:15 +0800

    Modified: Load stock df to bigquery table dag

--- airflow/dags/yahoo_api_dag.py
+++ airflow/dags/yahoo_api_dag.py
@@ -1,11 +1,13 @@
 from airflow import DAG
 from airflow.decorators import dag, task
-from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 from src.big_query import setup_client, load_dataframe_to_bigquery
-from google.cloud import bigquery
-from google.oauth2 import service_account
 import yfinance as yf
+
+# top 30 US tech firms by market cap
+ticker_list = ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA", "NVDA", "META", "AVGO", "ORCL", "CSCO", 
+                "CRM", "TXN", "ADBE", "NFLX", "QCOM", "AMD", "IBM", "INTU", "INTC", "AMAT",
+                "BKNG", "ADI", "ADP", "NOW", "PYPL", "ABNB", "FISV", "LRCX", "UBER", "EQIX"]
 
 default_args = {
     'owner':'airflow',
@@ -19,40 +21,24 @@ default_args = {
     start_date=datetime(2023, 1, 1),
     catchup=False,
     tags=['stock'],
+    max_active_tasks=10,
+    concurrency=10
 )
 def yahoo_api():
-
     @task
-    def fetch_stock_data(ticker, **kwargs): 
+    def load_stock_data(ticker, **kwargs): 
         ti = kwargs['ti']
         data = yf.download(tickers=ticker, period='30m', interval='5m')
         data = data.rename(columns={"Adj Close":"Adj_Close"})
-        print(data)
-        ti.xcom_push(f'stock_data_{ticker}', data)
-
-    @task
-    def update_bigquery_table(ticker, **kwargs):
-        ti = kwargs['ti']
-        stock_df = ti.xcom_pull(task_ids='fetch_stock_data', key=f'stock_data_{ticker}') 
-        table_id=f"Yahoo.{ticker}",
-        key_path = "../../token/is3107-grp18-e8944871c568.json" # use your own token
+        data['Datetime'] = data.index
+        stock_df = data.reset_index(drop=True)
+        print("#####" + ticker + "#####")
+        table_id=f"Yahoo.{ticker}"
+        key_path = "token/is3107-grp18-e8944871c568.json" # use your own token
         client = setup_client(key_path)
         load_dataframe_to_bigquery(client, table_id, stock_df)
 
-
-    # top 30 US tech firms by market cap
-    ticker_list = ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA", "NVDA", "META", "AVGO", "ORCL", "CSCO", 
-    "CRM", "TXN", "ADBE", "NFLX", "QCOM", "AMD", "IBM", "INTU", "INTC", "AMAT",
-    "BKNG", "ADI", "ADP", "NOW", "PYPL", "ABNB", "FISV", "LRCX", "UBER", "EQIX"]
-
     for ticker in ticker_list:
-        fetch_stock = fetch_stock_data(ticker)
-
-        fetch_stock
-
-    for ticker in ticker_list:
-        update_bigquery_table = update_bigquery_table(ticker)
-
-        update_bigquery_table
+        load_stock_data(ticker)
 
 yahoo_api_dag = yahoo_api()
