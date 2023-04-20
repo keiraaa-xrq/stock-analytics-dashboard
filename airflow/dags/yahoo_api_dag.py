8793aafedde50515216978ff98b46487faad1694
keiraaa-xrq <fantasy9923@gmail.com>
2023-04-20 13:48:22 +0800

    Modified yahoo api dag.

--- airflow/dags/yahoo_api_dag.py
+++ airflow/dags/yahoo_api_dag.py
@@ -1,24 +1,14 @@
+import os
 from airflow import DAG
 from airflow.decorators import dag, task
 import pandas as pd
 from datetime import datetime
-import os
-import time
 import pendulum
+import yfinance as yf
 from src.bigquery import setup_client, load_dataframe_to_bigquery
 from src.transform_yahoo import transform_stock_df, check_validity, query_table_time
-import yfinance as yf
-
-# top 30 US tech firms by market cap
-ticker_list = ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA", "NVDA", "META", "AVGO", "ORCL", "CSCO", 
-"CRM", "TXN", "ADBE", "NFLX", "QCOM", "AMD", "IBM", "INTU", "INTC", "AMAT",
-"BKNG", "ADI", "ADP", "NOW", "PYPL", "ABNB", "FISV", "LRCX", "UBER", "EQIX"]
-dataset_id="Yahoo_test.{}" # for testing only
-TOKEN_PATH = "key/service_account_key.json" # use your own token
-client = setup_client(TOKEN_PATH)
-stock_tz = pendulum.timezone("America/New_York")
-
-start_time = time.time() # for testing only
+from src.static import TICKER_LIST
+from src.utils import get_key_file_name
 
 default_args = {
     'owner':'airflow',
@@ -29,13 +19,16 @@ default_args = {
     default_args=default_args, 
     description='Fetch latest stock data',
     schedule_interval='*/30 9-16 * * MON-FRI',
-    start_date=datetime(2023, 1, 1, tzinfo=stock_tz),
+    start_date=datetime(2023, 1, 1, tzinfo=pendulum.timezone("America/New_York")),
     catchup=False,
     tags=['stock'],
     concurrency=16,
     max_active_runs=10
 )
-def yahoo_api():
+def yahoo_api_dag():
+    dataset_id = 'Yahoo.{}'
+    key_file = get_key_file_name()
+    client = setup_client(f'./key/{key_file}')
 
     @task(max_active_tis_per_dag=10)
     def extract_stock_data(ticker):
@@ -50,6 +43,7 @@ def yahoo_api():
         print(stock_df)
 
         table_id = dataset_id.format(ticker)
+        
         table_time = query_table_time(client, table_id)
 
         stock_df = transform_stock_df(stock_df)
@@ -67,25 +61,19 @@ def yahoo_api():
         if len(stock_data) > 0:
             load_dataframe_to_bigquery(client, table_id, stock_data)
 
-        ### for testing only
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-
-        print("elapsed_time=", elapsed_time)
 
     # Extract: call yahoo finance api to fetch stock data
     stock_json_list = []
-    for ticker in ticker_list:
+    for ticker in TICKER_LIST:
         stock_json = extract_stock_data(ticker)
         stock_json_list.append(stock_json)
 
     # Transform & Load: process stock data & load it to remote db
-    for i in range(len(ticker_list)):
-        ticker = ticker_list[i]
+    for i in range(len(TICKER_LIST)):
+        ticker = TICKER_LIST[i]
         stock_json = stock_json_list[i]
         stock_json_transformed = transform_stock_data(stock_json)
         load_stock_data(ticker, stock_json_transformed)
 
-yahoo_api_dag = yahoo_api()
+yahoo_api_dag()
 
