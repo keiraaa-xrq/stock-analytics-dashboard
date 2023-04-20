ed083c9864608ffc560306162f796f16a720e204
Feng Yuan <gdfy102217@gmail.com>
2023-04-20 11:41:04 +0800

    resolve conflicts

--- airflow/dags/src/scrape_tweets.py
+++ airflow/dags/src/scrape_tweets.py
@@ -26,23 +26,14 @@ def get_tweets(twitter_accounts: List[str], start_time: pendulum.DateTime, end_t
                 tweets_list.append({
                     'tweet_id': tweet.id, 
                     'content': tweet.rawContent,
-<<<<<<< HEAD
                     # converting to str since datetime is not JSON serialisable
                     'date': tweet.date.strftime(format='%Y-%m-%d %H:%M:%S%z'),  
-=======
-                    # converting to timestamp since datetime is not JSON serialisable
-                    'date': tweet.date.timestamp(),  
->>>>>>> 5187d045777a5a601e81686b415bd961cb93f491
                     'url': tweet.url, 
                     'username': tweet.user.username, 
                     'retweet_count': tweet.retweetCount, 
                     'like_count': tweet.likeCount, 
                     'quote_count': tweet.quoteCount,
-<<<<<<< HEAD
                     'time_pulled': end_time.to_datetime_string() 
-=======
-                    'time_pulled': end 
->>>>>>> 5187d045777a5a601e81686b415bd961cb93f491
                 })
         except Exception as e:
             print(e)
